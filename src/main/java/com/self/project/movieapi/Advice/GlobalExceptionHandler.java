3db0b23b834eff06f325fffa6ed648b1fb3cd257
Sciderus <huydska11@gmail.com>
2025-02-17 13:16:13 +0700

    add Global exception Handle

--- src/main/java/com/self/project/movieapi/Advice/GlobalExceptionHandler.java
+++ src/main/java/com/self/project/movieapi/Advice/GlobalExceptionHandler.java
@@ -1,4 +1,19 @@
 package com.self.project.movieapi.Advice;
 
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public String handleBadCredentialsException(BadCredentialsException e) {
+        return "Error: Invalid username or password.";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleGeneralException(Exception e) {
+        return "Error: " + e.getMessage();
+    }
 }
